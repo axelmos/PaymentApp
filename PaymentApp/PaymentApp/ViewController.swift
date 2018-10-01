629423146f63262747460eb82635993a8e8a4eb8
Mosiejko Axel <amosiejko@lanacion.com.ar>
2018-10-01 17:04:18 -0300

    First commit. All project files with pods installed.

---
+++ PaymentApp/PaymentApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaymentApp
+//
+//  Created by Mosiejko Axel (Producto Y Tecnologia) on 01/10/2018.
+//  Copyright © 2018 Mosiejko Axel (Producto Y Tecnologia). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
